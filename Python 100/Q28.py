12431aa895b498e311dfbbbe327ac49a7c3bf1a8
xLeviatian <xLeviatian@gmail.com>
2024-11-01 13:57:23 -0700

    converts 2 strings to int and adds

---
+++ Python 100/Q28.py
@@ -0,0 +1,7 @@
+# Define a function that can receive two integer numbers in string form
+# and compute their sum and then print it in console.
+
+def converts_to_string_and_add(x, y):
+    print(int(x) + int(y))
+
+converts_to_string_and_add(str(56), str(15))
