aa173ca3847da19fba292899a0e55693506321b6
xLeviatian <xLeviatian@gmail.com>
2024-11-01 13:59:00 -0700

    concat strings

---
+++ Python 100/Q29.py
@@ -0,0 +1,7 @@
+# Define a function that can accept two strings as input
+# and concatenate them and then print it in console.
+
+def concat_string(x, y):
+    print(x + y)
+
+concat_string('hello', 'there')
