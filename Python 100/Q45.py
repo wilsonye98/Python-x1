3d6171819e834cd5e8477f6d3ab6e913ced207d1
xLeviatian <xLeviatian@gmail.com>
2024-11-05 04:17:21 -0800

    class with printNationality as static method

---
+++ Python 100/Q45.py
@@ -0,0 +1,9 @@
+# Define a class named American which has a static method called printNationality.
+
+class American:
+
+    @staticmethod
+    def printNationality():
+        print('American')
+
+American.printNationality()
