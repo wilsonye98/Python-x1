6524e7baa3795a51f5bd3b485616b9ac52cbdb02
xLeviatian <xLeviatian@gmail.com>
2024-11-05 04:26:53 -0800

    Square with parent class shape, both has area method

---
+++ Python 100/Q49.py
@@ -0,0 +1,15 @@
+# Define a class named Shape and its subclass Square. The Square
+# class has an init function which takes a length as argument. Both
+# classes have a area function which can print the area of the shape
+# where Shape's area is 0 by default.
+
+class Shape:
+    def area(self):
+        return 0
+
+class Square(Shape):
+    def __init__(self, l):
+        self.length = l
+
+    def area(self):
+        return self.length ** 2
