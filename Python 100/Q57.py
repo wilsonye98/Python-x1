63d509c10aa526e9481c9020bfa89d991cf30370
xLeviatian <xLeviatian@gmail.com>
2024-11-06 18:25:31 -0800

    converting ascii to unicode

---
+++ Python 100/Q57.py
@@ -0,0 +1,7 @@
+# Write a program to read an ASCII string and to convert it to a
+# unicode string encoded by utf-8.
+def ascii_to_unicode(ascii):
+    unicode = ascii.encode(encoding='utf-8')
+    print(u'{}'.format(u))
+
+ascii_to_unicode('Hello world')
