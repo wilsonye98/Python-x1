5c4da67997607883a8d659512d345f7fc9e165fa
xLeviatian <xLeviatian@gmail.com>
2024-11-08 01:01:19 -0800

    5 random int between 1 and 1000 div by both 5 and 7

---
+++ Python 100/Q74.py
@@ -0,0 +1,7 @@
+# Please write a program to randomly generate a list with 5
+# numbers, which are divisible by 5 and 7 , between 1 and 1000 inclusive.
+
+import random
+
+rand_lst = random.sample([n for n in range(1, 1001) if n % 5 == 0 and n % 7 == 0], 5)
+print(rand_lst)
