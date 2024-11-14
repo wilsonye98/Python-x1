8b4c9a4aaae86bd436c2cb6d4987542c93721f12
xLeviatian <xLeviatian@gmail.com>
2024-11-14 14:32:05 -0800

    reverse string

---
+++ Python 100/Q91.py
@@ -0,0 +1,7 @@
+# Please write a program which accepts a string from console and
+# print it in reverse order.
+
+def reverse_str(string):
+    return string[::-1]
+
+print(reverse_str("rise to vote sir"))
